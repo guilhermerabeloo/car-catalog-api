9b235f48e8fb16ee3f9afd29063cd46fc1cdbd13
guilhermerabeloo <guilhermerabelo699@gmail.com>
2025-02-25 22:04:16 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando na Vercel!" });
+});
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "Pong!" });
+});
+
+// Porta definida automaticamente pela Vercel
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
