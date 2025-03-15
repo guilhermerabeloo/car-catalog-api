64936f507223af9a2027dec83c891b92908976d4
FerNegreiro <156912120+FerNegreiro@users.noreply.github.com>
2025-03-15 11:19:13 -0300

    Feat/Rota DELETE /excluirCarro

--- src/routes/carsCatalog.js
+++ src/routes/carsCatalog.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
     .get('/catalogoCarros', CarCatalogController.getAllCars)
-    .post('/cadastroCarro', CarCatalogController.createCar);
+    .post('/cadastroCarro', CarCatalogController.createCar)
+    .delete('/excluirCarro/:id', CarCatalogController.deleteCar)
 
 export default router;
